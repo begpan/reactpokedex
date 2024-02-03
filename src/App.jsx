0cbe0dc56687761f87dd68488bb6c4548c408282
begpan <begumpandora@gmail.com>
2024-02-03 13:30:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./Pokecard.css";
+import Pokegame from "./Pokegame";
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
